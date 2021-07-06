1646dc29a155f7391bba0b68428177c099d3765e
mubing.ye <mubing.ye>
2021-07-06 17:37:08 +0800

    init

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log(process.argv);
+const fs = require("fs");
+const mime = require("mime");
+
+const imgPaths = process.argv.slice(2);
+const res = [];
+if (imgPaths.length > 0) {
+  for (let src of imgPaths) {
+    const bitmap = fs.readFileSync(src);
+    const fileType = mime.getType(src);
+    const img = {
+      path: src,
+      base64: `data:${fileType};base64,${Buffer.from(bitmap, "binary").toString("base64")}`,
+    };
+    res.push(img);
+  }
+  console.log(res);
+} else {
+  console.log("\x1B[31m%s\x1B[0m", "enter img path");
+}
